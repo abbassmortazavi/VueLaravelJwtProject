e31b42f60ae89b0fbfddf4a9207789e9a358f2fb
Abbass Mortazavi <abbassmortazavi@gmail.com>
2021-08-30 20:11:37 +0430

    added project in to git

---
+++ front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <NavBar/>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import NavBar from './components/Navbar.vue';
+export default {
+  name: 'app',
+  components:{
+    NavBar
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+
+</style>
